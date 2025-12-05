350748b1de0a24e7faefdce3f1dccbff14e1f4eb
Leon <leon.hebeisen08@gmail.com>
2025-12-05 23:13:40 +0100

    [Update] Grid update

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function darkmode() {
 function submitMail() {
   alert("Thank you for your message. I will get back to you shortly!");
 }
+
+
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("open");
+}
+
+function darkmode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function submitMail() {
+    alert("Function for sending message not implemented yet.");
+}
+
